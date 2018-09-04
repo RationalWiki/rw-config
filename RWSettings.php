7ee53e740cf6fd2af2d63a20ee2909370a75208b
davidgerard <dgerard@gmail.com>
2018-09-04 20:33:40 +0100

    Board2018 election

--- RWSettings.php
+++ RWSettings.php
@@ -601,9 +601,9 @@ $wgShellLocale = "en_US.utf8";
 # Enable caching of DynamicPageList, otherwise the Varnish cache is suppressed for virtually every main namespace page
 ExtDynamicPageList::$respectParserCache = true;
 
-#$wgElectionName = "Mods2018";
-#$wgElectionCandidates = array("Bigs", "Bongolian", "CheeseburgerFace", "Christopher", "Cosmikdebris", "CowHouse", "DiamondDisc1", "Kazitor", "LeftyGreenMario", "Nerd", "Pbfreespace3", "Spud", "Xbony2");
-#$wgElectionStoreDir = "$IP/../election";
+$wgElectionName = "Board2018";
+$wgElectionCandidates = array("Bob M", "David Gerard", "Enchanted13", "Ikanreed", "LeftyGreenMario", "Pbfreespace3", "RobSmith");
+$wgElectionStoreDir = "$IP/../election";
 
 # Less annoying watchlist notifications
 $wgDefaultUserOptions['watchcreations'] = 1;
